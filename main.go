b3e08862cf6a2a2bbcab4b50f1340e92071068cc
Aiden Scott <aidenscott12@googlemail.com>
2020-03-17 22:53:06 +0000

    adds deck.tostring

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 3)
-	hand.print()
-	remainingCards.print()
+	// hand, remainingCards := deal(cards, 3)
+	fmt.Println(cards.toString())
 
 }
